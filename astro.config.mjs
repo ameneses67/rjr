246a045c280933e6a03e25ebe00594757172fbbc
Poncho <ameneses67@gmail.com>
2023-09-08 17:21:31 -0600

    git workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: "https://ameneses67.github.io",
+  integrations: [tailwind(), mdx()],
 });
