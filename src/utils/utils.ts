13a7e203fe3fe0169d21d5b3323fda0e03efff6b
Poncho <ameneses67@gmail.com>
2023-09-11 18:50:48 -0600

    generación de páginas para categorías

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,21 @@ export const currentYear = new Date().getFullYear();
 export const getFormattedDate = (date: Date) =>
   date
     ? new Date(date).toLocaleDateString("es-MX", {
+        timeZone: "UTC",
         year: "numeric",
         month: "long",
       })
     : "";
+
+export const slugify = (text: string) => {
+  return text
+    .normalize("NFD")
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "")
+    .replace(/[\u0300-\u036f]/g, "");
+};
