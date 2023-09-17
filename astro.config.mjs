96cbe9c791da08a4a8ff6b1bf3609dcaee872610
Poncho <ameneses67@gmail.com>
2023-09-16 18:41:20 -0600

    arreglando paginación

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ameneses67.github.io",
+  trailingSlash: "never",
   build: {
     format: "file",
   },
