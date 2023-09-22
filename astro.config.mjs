9218314b08646e1821605f553fd196f30f535fc3
Poncho <ameneses67@gmail.com>
2023-09-21 20:09:07 -0600

    configuración blog con ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rjr.pages.dev",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()],
+  output: "server",
+  adapter: vercel()
 });
