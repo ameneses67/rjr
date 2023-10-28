be44c3e8d8a48bc1d8423ca88eea67f7c57734f4
Poncho <ameneses67@gmail.com>
2023-10-28 11:44:58 -0600

    artículo objetivos educativos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rjr.pages.dev",
+  site: "http://localhost:4321",
   integrations: [
     tailwind({
       applyBaseStyles: false,
